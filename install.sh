b0ea0ea0ad5d5e76f0a7a0e043aae42300bbfaad
testdasi <51292604+testdasi@users.noreply.github.com>
2020-09-09 19:07:53 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install some additional packages
+apt-get -y update \
+    && apt-get -y install wget
+
+# install cloudflared
+# partially done in Dockerfile due to platform difference
+useradd -s /usr/sbin/nologin -r -M cloudflared \
+    && chown cloudflared:cloudflared /usr/local/bin/cloudflared
+
+# clean cloudflared config
+mkdir -p /etc/cloudflared \
+    && rm -f /etc/cloudflared/config.yml
+
+# add cloudflared config
+cd /tmp \
+    && mkdir -p /etc/cloudflared \
+    && cp -n ./config.yml /etc/cloudflared/ \
+    && rm -f ./config.yml
+
+# clean up
+apt-get -y remove wget \
+    && apt-get -y autoremove \
+    && apt-get -y autoclean \
+    && apt-get -y clean \
+    && rm -fr /tmp/* /var/tmp/* /var/lib/apt/lists/*
